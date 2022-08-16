9b548c4acaf842d0697245a1b8d204f49e84894b
pingleee <v65224@gmail.com>
2022-08-16 15:23:08 +0900

    answer

--- reviews.py
+++ reviews.py
@@ -8,6 +8,7 @@ with open('reviews.txt', 'r') as f:
             print(len(data))
 print('Finished. Total amount is :', len(data))
 
+# My tried ver.
 t = 0
 total = 0
 while t < len(data):
@@ -15,3 +16,9 @@ while t < len(data):
     t = t + 1
 average = total / len(data)
 print(average)
+
+# Answer
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+print('Len Average is ', sum_len/len(data))
